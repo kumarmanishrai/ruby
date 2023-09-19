b24ae085cef8c625382ce440d116412c851091bc
ManishKumarRai-creator <98271764+kumarmanishrai@users.noreply.github.com>
2023-09-19 20:11:07 +0530

    done with strings

---
+++ strings.rb
@@ -0,0 +1,13 @@
+
+name = "Manish"
+
+puts name.upcase
+puts name.downcase 
+puts name.length
+puts name.include? "Manish"
+puts name[2]
+puts name[0, 3]
+puts name.index("an")
+
+name2 = "  Manish Rai   "
+puts name2.strip
