fe915f34fd672f7a4ce72da9e1c3dc56f94791cd
ManishKumarRai-creator <98271764+kumarmanishrai@users.noreply.github.com>
2023-09-19 20:33:22 +0530

    added numbers.rb

---
+++ numbers.rb
@@ -0,0 +1,14 @@
+puts 2**3
+
+num = 2
+puts ("manish" + num.to_s)
+
+num2 = -34
+puts num2.abs()
+
+num3 = 6.78
+puts "round: " + num3.round().to_s 
+puts "floor: " + num3.floor().to_s 
+puts "ceil: " + num3.ceil().to_s  
+
+puts Math.sqrt(36)
