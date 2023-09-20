5e0c0a9cbecd35e8bf2f763605fd3e94f16e1f26
ManishKumarRai-creator <98271764+kumarmanishrai@users.noreply.github.com>
2023-09-20 15:20:51 +0530

    added input.rb

---
+++ input.rb
@@ -0,0 +1,14 @@
+# puts "Enter Your name: "
+# name = gets.chomp()
+# puts "Enter Your age: "
+# age = gets.chomp()
+# puts ("Hello "+name+ ", you are " + age + " years old")
+
+puts "I am a calculator "
+puts "Enter num1: "
+num1 = gets.chomp()
+puts "Enter num2: "
+num2 = gets.chomp()
+
+# puts(num1.to_i + num2.to_i)
+puts(num1.to_f + num2.to_f)
